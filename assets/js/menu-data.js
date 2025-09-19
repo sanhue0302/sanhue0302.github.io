31c3db3444bc3857af6c05e83c25e32e928d0e2f
Sanhue <sanhue@gmail.com>
2025-09-20 01:07:26 +0800

    網頁改為展示空間

---
+++ assets/js/menu-data.js
@@ -0,0 +1,30 @@
+const menuData = [
+  {
+    text: 'Home',
+    icon: 'fas fa-home',
+    link: 'pages/home.html'
+  },
+  {
+    text: 'Games',
+    icon: 'fas fa-gamepad',
+    submenu: [
+      {
+        text: 'Arch',
+        link: 'games/arch.html'
+      },
+      {
+        text: 'Dada',
+        link: 'games/dada.html'
+      },
+      {
+        text: 'TD_N',
+        link: 'games/TD_N.html'
+      }
+    ]
+  },
+  {
+    text: 'About',
+    icon: 'fas fa-info-circle',
+    link: 'pages/about.html'
+  }
+];
---
+++ assets/js/seo-data.js
@@ -0,0 +1,13 @@
+const seoData = {
+    title: "Sanhue0302的個人空間",
+    description: "一位軟體工程師的 Side Project 展示空間，包含與 AI 協作開發的趣味小遊戲。",
+    og: {
+        title: "Sanhue0302的個人空間",
+        description: "逛逛我的 Side Project、看看我的技術實踐，或來玩玩看跟 AI 一起做的小遊戲！",
+        type: "website",
+        // Remember to change this to your actual GitHub Pages URL
+        url: "https://sanhue0302.github.io/sanhue0302/", 
+        // You can add an image URL here to be used as a preview
+        image: ""
+    }
+};
